0777c4711579bde0c88cbde6f4ab8e686cf4b157
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2023-09-29 15:32:02 +0200

    XML gen

--- src/demo/src/App.vue
+++ src/demo/src/App.vue
@@ -6,20 +6,30 @@
   </nav>
 
   <div id="container">
-    <div v-if="!isHTML" id="textfield">{{ text }}</div>
-    <div v-else id="textfield" v-html="html"></div>
+    <div v-if="!isXML" id="textfield" :contenteditable="editMode">{{ loremText }}</div>
+    <div v-else id="textfield" v-html="loremXML" :contenteditable="editMode"></div>
 
     <div id="controls">
       <div id="text-html">
         <p>TEXT</p>
         <label class="switch">
-          <input type="checkbox" v-model="isHTML" />
+          <input type="checkbox" v-model="isXML" />
           <span class="slider"></span>
         </label>
-        <p>HTML</p>
+        <p>XML</p>
       </div>
-      <button @click="editText">EDIT MANUALLY</button>
+      <label v-if="isXML" id="depth">
+        Maximální zanoření ({{ maxDepth }})
+        <input type="range" min="1" max="10" v-model="maxDepth" />
+      </label>
       <button @click="generateText">GENERATE</button>
+      <button @click="editText">EDIT MANUALLY <span v-if="editMode">✅</span><span v-else>❌</span></button>
+      <button @click="createAnchor">CREATE ANCHOR</button>
+    </div>
+
+    <div id="anchors">
+      <h3>Anchors</h3>
+      <div v-for="anchor in anchors" :key="anchor">{{ anchor }}</div>
     </div>
   </div>
 </template>
@@ -28,28 +38,58 @@
 
 <script setup>
 import { ref } from "vue";
-import { LoremIpsum } from "lorem-ipsum";
 //
-import * as DTA from "../../../dist/index.js";
+import { LoremIpsum } from "lorem-ipsum";
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: { max: 16, min: 3, },
+  wordsPerSentence: { max: 9, min: 3, },
+});
+//
+import { DTA } from "../../../dist/index.js";
 
-DTA.hello();
+const loremText = ref("");
+const loremXML = ref("");
+const isXML = ref(false);
+const maxDepth = ref(1);
+const editMode = ref(false);
+const anchors = ref([]);
+//
+const dta = new DTA();
 
-const isHTML = ref(false);
-const text = ref("Lorem ipsum dolor sit amet...");
-const html = ref("<p>Lorem ipsum dolor sit amet...</p>");
+function randomInt(min = 0, max = 1) { return Math.floor(Math.random() * (max - min + 1)) << 0 }
 
-function editText() { }
+function genLoremXML(depth = 0) {
+  let paragraphs = lorem.generateParagraphs(randomInt(1, Math.round(16 / depth))).split("\n"); // decrease number of paragraphs with greater depth
+  paragraphs = paragraphs.map((p) => {
+    if (Math.random() > 0.5 && depth < maxDepth.value) { // 50% chance of creating child paragraph
+      let sentences = p.split(/[\.\!\?]\s/);
+      sentences = sentences.slice(0, randomInt(0, sentences.length));
+      const splitIndex = sentences.join(". ").length + 2; // + 2 because of the interpuction and space after the last sentence
+      return `<div><p>${p.substring(0, splitIndex)}</p>${genLoremXML(depth + 1)}<p>${p.substring(splitIndex)}</p></div>`;
+    } else return `<p>${p}</p>`;
+  });
+  return paragraphs.map((p) => p.replaceAll(/\<p\>\s*\<\/p\>/g, "")).join(""); // remove empty paragraphs
+}
 
 function generateText() {
-  const randomInt = (min = 0, max = 1) => Math.floor(Math.random() * (max - min + 1)) << 0;
+  if (isXML.value) {
+    do {
+      loremXML.value = genLoremXML();
+    } while (loremXML.value === "");
+    dta.setXML(loremXML.value);
+  } else {
+    do {
+      loremText.value = lorem.generateParagraphs(randomInt(1, 16));
+    } while (loremText.value === "");
+    dta.setText(loremText.value);
+  }
+}
 
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: { max: 16, min: 3, },
-    wordsPerSentence: { max: 9, min: 3, },
-  });
+function editText() {
+  editMode.value = !editMode.value;
+}
 
-  let loremText = lorem.generateParagraphs(randomInt(1, 16));
-  if (isHTML.value) html.value = `<p>${loremText.split("\n").join("</p><p>")}</p>`; // možnost volby max zanoření
-  else text.value = loremText;
+function createAnchor() {
+  anchors.value.push("ANCHOR#" + new Date().getTime());
 }
 </script>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,22 @@
-export function hello() {
-    let message: string = "Hello World";
-    console.log(message);
+export class DTA {
+    // rawText: string;
+    rawXML: Document;
+
+    constructor() {}
+
+    public setText(text: string) {
+        text = `<p>${text}</p>`;
+        this.rawXML = this.validateXML(text);
+    }
+
+    public setXML(xml: string) {
+        this.rawXML = this.validateXML(xml);
+    }
+
+    private validateXML(xml: string) {
+        const xmlDoc: Document = new DOMParser().parseFromString(xml, "text/xml");
+        const errNode = xmlDoc.querySelector("parsererror");
+        if (errNode) throw new Error("Invalid XML!\n" + errNode.textContent);
+        return xmlDoc;
+    }
 }
