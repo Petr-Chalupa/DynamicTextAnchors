a3ff7ae3f742738a5ee12053f3613bef13f50a39
Petr Chalupa <petr.chalupa@student.gyarab.cz>
2023-11-06 10:18:02 +0100

    Random special tags insertion

--- src/demo/src/App.vue
+++ src/demo/src/App.vue
@@ -17,6 +17,7 @@
         </label>
         <p>USE</p>
       </div>
+      <LoremGenSettings v-model="loremGenSettings" />
       <label id="depth">
         Maximal depth ({{ maxDepth }})
         <input type="range" min="1" max="10" v-model="maxDepth" />
@@ -45,12 +46,18 @@
 <script setup>
 import { ref, watch } from "vue";
 import { LoremIpsum } from "lorem-ipsum";
-const lorem = new LoremIpsum({
+import { DTA } from "../../../dist/index.js";
+import LoremGenSettings from "./LoremGenSettings.vue";
+import * as Icon from "./assets/icon.svg";
+
+const loremGenSettings = ref({
   sentencesPerParagraph: { max: 16, min: 3, },
   wordsPerSentence: { max: 9, min: 3, },
+  specialTags: {
+    text: ["h1", "h2", "h3", "h4", "h5", "h6", "i", "b", "u"],
+    nontext: ["img", "audio", "video", "table"],
+  }
 });
-import { DTA } from "../../../dist/index.js";
-
 const textfield = ref(null);
 const loremXML = ref("");
 const maxDepth = ref(5);
@@ -70,20 +77,38 @@ watch(useMode, () => {
 
 function randomInt(min = 0, max = 1) { return Math.floor(Math.random() * (max - min + 1)) << 0 }
 
-function genLoremXML(depth = 0) { //insert random <b>, <i>, <img>... tags (todo)
-  let paragraphs = lorem.generateParagraphs(randomInt(1, Math.round(16 / depth))).split("\n"); // decrease number of <p> with greater depth
-  paragraphs = paragraphs.map((p) => {
-    if (Math.random() > 0.5 && depth < maxDepth.value) { // 50% chance of creating child <div>
-      let sentences = p.split(/[\.\!\?]\s/);
-      sentences = sentences.slice(0, randomInt(0, sentences.length)); // random position of child <div> inside parent <div>
-      const splitIndex = sentences.join(". ").length + 2; // + 2 because of the ". " after the last sentence
-      return `<div><p>${p.substring(0, splitIndex)}</p>${genLoremXML(depth + 1)}<p>${p.substring(splitIndex)}</p></div>`;
-    } else return `<p>${p}</p>`;
-  });
-  return paragraphs.map((p) => p.replaceAll(/\<(p|div)\>\s*\<\/(p|div)\>/g, "")).join(""); // remove empty <p>
-}
-
 function generateXML() {
+  const lorem = new LoremIpsum({
+    ...loremGenSettings.value.sentencesPerParagraph,
+    ...loremGenSettings.value.wordsPerSentence,
+  }, "plain", "\n");
+
+  const genRandomTag = (value = "") => {
+    let tag = ["p", "div", "span"][randomInt(0, 2)]; // default normal tag
+    let isText = true;
+    if (randomInt(0, 4) === 0) { // 25% chance for special tag
+      if (randomInt(0, 8) === 0) { // 6,25% chance for special non-text tag
+        isText = false;
+        tag = loremGenSettings.value.specialTags.nontext[randomInt(0, loremGenSettings.value.specialTags.nontext.length - 1)];
+      }
+      else tag = loremGenSettings.value.specialTags.text[randomInt(0, loremGenSettings.value.specialTags.text.length - 1)];
+    }
+    return isText ? `<${tag}>${value}</${tag}>` : `<${tag} controls="true" src="${Icon.default}" title="${value}" />`;
+  };
+
+  const genLoremXML = (depth = 0) => {
+    let paragraphs = lorem.generateParagraphs(randomInt(1, Math.round(16 / depth))).split("\n"); // decrease number of <p> with greater depth
+    paragraphs = paragraphs.map((p) => {
+      if (Math.random() > 0.5 && depth < maxDepth.value) { // 50% chance of creating child <div>
+        let sentences = p.split(/[\.\!\?]\s/);
+        sentences = sentences.slice(0, randomInt(0, sentences.length)); // random position of child <div> inside parent
+        const splitIndex = sentences.join(". ").length + 2; // + 2 because of the ". " after the last sentence
+        return `<div>${genRandomTag(p.substring(0, splitIndex))}${genLoremXML(depth + 1)}${genRandomTag(p.substring(splitIndex))}</div>`;
+      } else return p;
+    });
+    return paragraphs.join("");
+  };
+
   do {
     loremXML.value = genLoremXML();
   } while (loremXML.value.trim().length === 0);
